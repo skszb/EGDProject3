8da91b1f69a925446c108e4a8d9397bd134c7c20
GSKcameraman <gskcameraman@gmail.com>
2023-02-01 00:20:56 -0500

    random sound

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,7 +10,42 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     public AudioSource audioSource;
-    public AudioClip audioClip;
+
+    [Header("Audios")]
+    public int beatCount = 4;
+    int currentBeat = 0;
+    bool enabled = true;
+    float interval = 0;
+    public TextMeshProUGUI bpmText;
+
+    [Header("WoddenFish")]
+    public AudioClip WoodenFishClip;
+
+    [Header("CowBell")]
+    public AudioClip cowBellClip;
+    [Range(0.0f, 100.0f)]
+    public float chanceCowBell = 10.0f;
+
+    [Header("Snare 808")]
+    public AudioClip snareClip;
+    [Range(0.0f, 100.0f)]
+    public float chanceSnare = 10.0f;
+
+
+    [Header("Clap 808")]
+    public AudioClip clapClip;
+    [Range(0.0f, 100.0f)]
+    public float chanceClap = 10.0f;
+
+
+    [Header("Bell")]
+    public AudioClip bellClip;
+    [Range(0.0f, 100.0f)]
+    public float chanceBell = 5.0f;
+
+    [Space]
+
+
 
     public Transform meritText;
     public Transform meritTextParent;
@@ -38,26 +73,79 @@ public class GameManager : MonoBehaviour
         else {
             randomTextLength = randomTexts.Length;
         }
+
+        audioSource.clip = WoodenFishClip;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        interval += Time.deltaTime;
     }
 
     public void HitWoodenFish()
     {
-        audioSource.PlayOneShot(audioClip);
 
-        Transform mt = Instantiate(meritText, meritTextParent);
-        mt.gameObject.SetActive(true);
-
-        hitAmountText.text = $"Total Merits: {++hitAmount}";
-
-        if (_randomTextCoroutine == null)
+        if (enabled)
         {
-            _randomTextCoroutine = StartCoroutine(RandomText());
+
+            if (currentBeat >= beatCount)
+            {
+                float rand = Random.Range(0.0f, 100.0f);
+                if (rand < chanceBell)
+                {
+                    audioSource.clip = bellClip;
+                    audioSource.Play();
+                    enabled= false;
+                    bpmText.SetText("0");
+                    return;
+                }
+                else if (rand < chanceBell + chanceCowBell)
+                {
+                    audioSource.clip = cowBellClip;
+                }
+                else if (rand < chanceBell + chanceCowBell + chanceSnare)
+                {
+                    audioSource.clip = snareClip;
+                }
+                else if (rand < chanceBell + chanceCowBell + chanceSnare + chanceClap)
+                {
+                    audioSource.clip = clapClip;
+                }
+
+                else
+                {
+                    audioSource.clip = WoodenFishClip;
+                }
+                currentBeat = 0;
+            }
+
+
+            audioSource.Play();
+
+            float bpm = 60 / interval;
+
+            bpmText.SetText(Mathf.Round(bpm).ToString());
+
+            interval = 0.0f;
+
+            currentBeat++;
+
+            Transform mt = Instantiate(meritText, meritTextParent);
+            mt.gameObject.SetActive(true);
+
+            hitAmountText.text = $"Total Merits: {++hitAmount}";
+
+            if (_randomTextCoroutine == null)
+            {
+                _randomTextCoroutine = StartCoroutine(RandomText());
+            }
+
+        }
+        if (!enabled && !audioSource.isPlaying)
+        {
+            enabled = true;
+            currentBeat = beatCount;
         }
     }
 
