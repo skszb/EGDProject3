263d43b37f82be957310732cab1e4fb7c3c7f1a8
jackyxu866 <33426411+JackyXu866@users.noreply.github.com>
2023-01-31 22:33:03 -0500

    randomText

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -15,11 +16,25 @@ public class GameManager : MonoBehaviour
     public Text hitAmountText;
     public int hitAmount;
 
+    // For random text
+    public string[] randomTexts;
+    public float[] randomTextsTime;
+    private int randomTextLength;
+    [SerializeField] public GameObject randomText;
+
     // Start is called before the first frame update
     void Start()
     {
         hitAmount = PlayerPrefs.GetInt(key: "HitAmount");
         hitAmountText.text = $"Total Merits: {PlayerPrefs.GetInt(key: "HitAmount")}";
+
+        if(randomTexts.Length != randomTextsTime.Length){
+            Debug.LogError("Random Texts and Random Texts Time are not the same length!");
+            randomTextLength = Mathf.Min(randomTexts.Length, randomTextsTime.Length);
+        }
+        else {
+            randomTextLength = randomTexts.Length;
+        }
     }
 
     // Update is called once per frame
@@ -36,10 +51,26 @@ public class GameManager : MonoBehaviour
         mt.gameObject.SetActive(true);
 
         hitAmountText.text = $"Total Merits: {++hitAmount}";
+
+        StartCoroutine(RandomText());
     }
 
     private void OnDestroy()
     {
         PlayerPrefs.SetInt("HitAmount", hitAmount);
+    }
+
+    IEnumerator RandomText(){
+        // pick a random text and time from the arrays
+        int randomTextIndex = Random.Range(0, (randomTextLength-1)*10);
+
+        if(randomTextIndex >= randomTextLength){
+            yield break;
+        }
+
+        randomText.SetActive(true);
+        randomText.GetComponentInChildren<TMP_Text>().text = randomTexts[randomTextIndex];
+        yield return new WaitForSeconds(randomTextsTime[randomTextIndex]);
+        randomText.SetActive(false);
     }
 }
